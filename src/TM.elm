2a952dc0d49f8dbf67f4cff2026b9c62ccbb358d
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-03-24 10:28:17 +0100

    Create a Model

--- src/TM.elm
+++ src/TM.elm
@@ -5,3 +5,45 @@ import Html exposing (text)
 main: Html.Html msg
 main =
     text "Hello, World!"
+
+
+-- Model
+
+
+type alias Model =
+    {
+      tm: TuringMachine Int String
+    , running: Bool
+    }
+
+
+type alias TuringMachine states symbols =
+    {
+      tape: Tape symbols
+    , state: states
+    , transitions: Transitions states symbols
+    }
+
+
+type alias Tape symbols =
+    {
+      left: List symbols
+    , head: symbols
+    , right: List symbols
+    }
+
+
+type alias Transitions states symbols =
+    List (Transition states symbols)
+
+
+type alias Transition states symbols =
+    {
+      current: (states, symbols)
+    , next: (states, symbols, Move)
+    }
+
+
+type Move =
+      Left
+    | Right
